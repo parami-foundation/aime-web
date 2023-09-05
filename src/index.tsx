fc40890462834f7207cdb45a119b8484dcebd13c
Kai Kang <cctv2206@outlook.com>
2023-09-05 14:44:33 +0800

    MOD: init app

---
+++ src/index.tsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import {
+  EthereumClient,
+  w3mConnectors,
+  w3mProvider
+} from '@web3modal/ethereum';
+import { Web3Modal } from '@web3modal/react';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import { goerli } from 'wagmi/chains';
+import App from './App.tsx';
+import './index.scss';
+import { ClerkProvider } from "@clerk/clerk-react";
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'hnft-badge': any
+    }
+  }
+}
+
+const chains = [goerli]
+const projectId = '2e586b0807500a0da3a4f7b66418295e';
+const INFURA_API_KEY = '46cdd1b1481049b992a90914cc17b52f';
+
+const { publicClient } = configureChains(
+  chains,
+  [w3mProvider({ projectId })]
+  // [infuraProvider({ apiKey: INFURA_API_KEY }), publicProvider()]
+);
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors: w3mConnectors({ projectId, chains } as any),
+  publicClient
+})
+const ethereumClient = new EthereumClient(wagmiConfig, chains)
+
+// const wagmiConfig = createConfig({
+//   autoConnect: true,
+//   connectors: [
+//     new MetaMaskConnector({ chains }),
+//     ...w3mConnectors({ projectId, version: 1, chains }),
+//   ],
+//   publicClient,
+//   // webSocketPublicClient,
+// });
+
+// const ethereumClient = new EthereumClient(wagmiConfig, chains);
+
+if (!process.env.REACT_APP_CLERK_PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+const clerkPubKey = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <>
+    <WagmiConfig config={wagmiConfig}>
+      <ClerkProvider publishableKey={clerkPubKey}>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<App />}>
+              {/* <Route path='' element={<AIME />} /> */}
+              {/* <Route path='' element={<MyHNFT />} /> */}
+              {/* <Route path='/issue' element={<IssueToken />} /> */}
+              {/* <Route path='/bid' element={<BidHNFT />} /> */}
+              {/* <Route path='/reward' element={<Reward />} /> */}
+              {/* <Route path='/reward/withdraws' element={<Withdraws />} /> */}
+              {/* <Route path='/claim' element={<ClaimAd />} /> */}
+
+              {/* <Route path='/aime' element={<LoginTest />} /> */}
+              {/* <Route path='/aime' element={<AIME />} />
+              <Route path='/aime/:handle' element={<AIME />} />
+
+              <Route path='/aime/bid/:handle' element={<BidAiAd />} />
+              <Route path='/aime/powers' element={<AiPowerReward />} />
+
+              <Route path='/login' element={<TestPage />} /> */}
+
+              <Route path='*' element={<Navigate to='/' />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </ClerkProvider>
+    </WagmiConfig>
+
+    <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+  </>
+);
